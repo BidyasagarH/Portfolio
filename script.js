087338a2bf4db39cf954598370d473c477fa90a1
BidyasagarH <88052746+BidyasagarH@users.noreply.github.com>
2023-04-09 04:10:14 +0530

    Added  portfolio website files

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Frontend Developer","Graphic Designer","Freelancer","Content writer","UI/UX Designer"],
+    typeSpeed:50,
+    backSpeed:10,
+    backDelay:1000,
+    loop:true
+})
+
